da68f482e42ff26e9b2689cae561105fba5213fa
Kyle Givler <6787154+JoyfulReaper@users.noreply.github.com>
2026-07-25 15:56:08 -0400

    Add built-in server time page

--- HappyGopher/Program.cs
+++ HappyGopher/Program.cs
@@ -30,11 +30,14 @@ builder.Services
 builder.Services.AddMissionControlClient(
     builder.Configuration.GetSection(
         MissionControlClientOptions.SectionName));
-
+builder.Services.AddSingleton<TimeProvider>(TimeProvider.System);
 builder.Services.AddSingleton<GopherContentStore>();
 builder.Services.AddScoped<GopherPageResolver>();
 builder.Services.AddTcpServer<GopherConnectionHandler, HappyGopherOptions>();
 builder.Services.AddHostedService<GopherLifecycleService>();
+
+// Currently all pages must be registered here.
+builder.Services.AddScoped<IGopherPage, ServerTimePage>();
 
 var host = builder.Build();
 host.Run();
